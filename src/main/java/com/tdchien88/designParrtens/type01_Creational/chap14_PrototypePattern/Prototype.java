b78032b4d2d1da3b204c89ba2dfc07d6a4ea9c12
TDChien88 <tdchien88@gmail.com>
2020-02-23 16:29:37 +0900

    prototype

---
+++ src/main/java/com/tdchien88/designParrtens/type01_Creational/chap14_PrototypePattern/Prototype.java
@@ -0,0 +1,7 @@
+package com.tdchien88.designParrtens.type01_Creational.chap14_PrototypePattern;
+
+public interface Prototype {
+
+	public Prototype getClone();
+
+}// End of Prototype interface.
--- src/main/java/com/tdchien88/designParrtens/type01_Creational/chap14_PrototypePattern/packageInfo.java
+++ src/main/java/com/tdchien88/designParrtens/type01_Creational/chap14_PrototypePattern/packageInfo.java
@@ -1,5 +1,40 @@
 package com.tdchien88.designParrtens.type01_Creational.chap14_PrototypePattern;
+// https://www.javatpoint.com/prototype-design-pattern
 
+/**
+ * Prototype Pattern says that cloning of an existing object instead of creating
+ * new one and can also be customized as per the requirement.
+ * 
+ * This pattern should be followed, if the cost of creating a new object is
+ * expensive and resource intensive.
+ * 
+ * Advantage of Prototype Pattern
+ * 
+ * The main advantages of prototype pattern are as follows:
+ * 
+ * It reduces the need of sub-classing.
+ * 
+ * It hides complexities of creating objects.
+ * 
+ * The clients can get new objects without knowing which type of object it will
+ * be.
+ * 
+ * It lets you add or remove objects at runtime.
+ * 
+ * # Usage of Prototype Pattern
+ * 
+ * _ When the classes are instantiated at runtime.
+ * 
+ * _ When the cost of creating an object is expensive or complicated.
+ * 
+ * _ When you want to keep the number of classes in an application minimum.
+ * 
+ * _ When the client application needs to be unaware of object creation and
+ * representation.
+ * 
+ * @author kyotran
+ *
+ */
 public class packageInfo {
 
 }
